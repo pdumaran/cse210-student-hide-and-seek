b880bc1e54055adf1ce10c9d42a5c3f3ebd1d945
pdumaran <75081496+pdumaran@users.noreply.github.com>
2021-10-11 20:45:13 +0800

    update

--- hide_and_seek/game/hider.py
+++ hide_and_seek/game/hider.py
@@ -1,3 +1,45 @@
 import random
 
-# TODO: Define the Hider class here
+class Hider:
+    """A code template for our hider. The responsibility of this class of
+    objects is to watch the seeker and give hints.
+    
+    Stereotype:
+        Information Holder
+    Attributes:
+        location (integer): The location of the hider (1-1000).
+        distance (list): The distance from the seeker.
+    """
+
+    def __init__(self):
+        """Class constructor. Declares and initializes instance attributes.
+        Args:
+            self (Hider): An instance of Hider.
+        """
+        self.location = random.randint(1, 1000)
+        self.distance = [0, 0] # start with two so get_hint always works
+    
+    def get_hint(self):
+        """Gets a hint for the hunter.
+        Args:
+            self (Hider): An instance of Hider.
+        
+        Returns:
+            string: A hint for the seeker.
+        """
+        hint = "(-.-) Maybe I'll take a nap."
+        if self.distance[-1] == 0:
+            hint = "(;.;) You found me!"
+        elif self.distance[-1] > self.distance[-2]:
+            hint = "(^.^) Getting colder!"
+        elif self.distance[-1] < self.distance[-2]:
+            hint = "(>.<) Getting warmer!"
+        return hint
+        
+    def watch(self, location):
+        """Watches the given location by keeping track of how far away it is.
+        Args:
+            self (Hider): An instance of Hider.
+        """
+        distance = abs(self.location - location)
+        self.distance.append(distance)
